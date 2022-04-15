e85bfb9f3ef48c700dc39e5381ce4ce1e54ce1f4
AnanyaGupta-pro <94893444+AnanyaGupta-pro@users.noreply.github.com>
2022-04-15 15:05:15 +0400

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,50 @@
+status="";
+
+objects=[];
+objectDetector=''
+img=""
+function preload(){
+    img=loadImage("ac.jpeg")
+      }
+      function setup() {
+        canvas = createCanvas(380, 380);
+        canvas.center();
+        objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+        document.getElementById("status").innerHTML="Status : Detecting Objects";
+     }
+     function modelLoaded(){
+        console.log("Model Loaded")
+        status=true;
+        objectDetector.detect(img,gotResult);
+    }
+    function gotResult(error,results){
+        if(error){
+            console.log(error)
+        }
+        console.log(results);
+        objects=results;
+        }
+        function back(){
+window.location="index.html";
+        }
+function draw(){
+    image(img,0,0,380,380);
+
+    if(status !=""){
+      r=random(255);
+      g=random(255);
+      b=random(255);
+     
+for (i=0;i<objects.length; i++){
+  document.getElementById("status").innerHTML="Status :  Objects Detected";
+
+fill(r,g,b)
+percent=floor(objects[i].confidence*100)
+text(objects[i].label+ " " +percent+"%",objects[i].x,objects[i].y);
+noFill();
+stroke(r,g,b);
+rect (objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+}
+    }
+}
